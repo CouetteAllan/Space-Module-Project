dd11176e01223e1b15c715366007c6449f4e3e34
SkyLaugh987 <55735176+CouetteAllan@users.noreply.github.com>
2023-10-17 14:05:57 +0200

    Update BasicEnemySpawner.cs

--- Assets/BasicEnemySpawner.cs
+++ Assets/BasicEnemySpawner.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class BasicEnemySpawner : MonoBehaviour
 {
     public GameObject EnemyPrefab;
+    private int _tickNeed;
 
     private void Awake()
     {
+        _tickNeed = 12;
         TimeTickSystemDataHandler.OnTick += TimeTickSystemDataHandler_OnTick;
+        GameManager.OnLevelUp += GameManager_OnLevelUp;
+        
+    }
+
+    private void GameManager_OnLevelUp(uint level)
+    {
+        switch (level)
+        {
+            case 8:
+                _tickNeed = 10;
+                return;
+            case 12:
+                _tickNeed = 8;
+                return;
+            case 18:
+                _tickNeed = 6;
+                return;
+            case 24:
+                _tickNeed = 4;
+                return;
+        }
     }
 
     private void TimeTickSystemDataHandler_OnTick(uint tick)
     {
-        if(tick % 12 == 0)
+        if(tick % _tickNeed == 0)
         {
             var enemy = Instantiate(EnemyPrefab,this.transform.position,Quaternion.identity);
             enemy.GetComponent<EnemyScript>().SetUpEnemy();
