1fb1219656a8ba05666c00b90f9e48cf854fe2da
COUETTE Allan <couetteallan@gmail.com>
2023-12-11 00:30:13 +0100

    Gather scrap visual feedback

--- Assets/Scripts/ScrapMetal.cs
+++ Assets/Scripts/ScrapMetal.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class ScrapMetal : MonoBehaviour
 {
-    public int _value = 1;
+    [SerializeField] private CircleCollider2D _collider;
+    private int _value = 1;
+    private Rigidbody2D _rigidbody2D;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent(out IGatherScrap player))
         {
-            player.GatherScrapMetal(_value);
-            Destroy(this.gameObject);
+            StartCoroutine(ScrapCoroutine(collision.gameObject.transform,player));
         }
     }
 
+    private IEnumerator ScrapCoroutine(Transform playerPos, IGatherScrap player)
+    {
+        _collider.enabled = false;
+        Vector2 startPos = _rigidbody2D.position;
+        float startTime = Time.time;
+        float endTime = startTime + 0.25f;
+
+        //repel the scrap
+        while(Time.time < endTime)
+        {
+            _rigidbody2D.velocity = (startPos - (Vector2)playerPos.position) * 6.0f;
+            yield return null;
+        }
+
+        endTime = Time.time + 0.4f;
+        while (Time.time < endTime)
+        {
+            _rigidbody2D.position = Vector2.Lerp(_rigidbody2D.position, playerPos.position,5.0f * Time.deltaTime);
+            yield return null;
+        }
+
+        player.GatherScrapMetal(_value);
+        Destroy(this.gameObject);
+        yield break;
+    }
     
 
     public void SetScrapValue(int value)
     {
         this._value = value;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 }
