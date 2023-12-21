84d2a14ae8e6b12e324ab0817ee972f67b139a1f
COUETTE Allan <couetteallan@gmail.com>
2023-12-21 15:52:02 +0100

    Obstacle get destroy

---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : MonoBehaviour, IObstacle
+{
+    [SerializeField] private Rigidbody2D _rb;
+    private int _health = 3;
+    
+    public void TryHit(IDamageSource source, int damage)
+    {
+        //Feedback hit
+        //Add force ?
+        _rb.AddForce(((Vector2)source.Transform.position - _rb.position) * source.RecoilMultiplier);
+        _health--;
+        if(_health <=0)
+            Destroy(gameObject);
+    }
+}
