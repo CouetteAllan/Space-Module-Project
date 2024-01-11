9c8593aaa416d32251dfb0add754599b13f42786
COUETTE Allan <couetteallan@gmail.com>
2024-01-11 15:29:50 +0100

    debut wave management

--- Assets/Scripts/SO/WaveSO.cs
+++ Assets/Scripts/SO/WaveSO.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveSO : MonoBehaviour
+[CreateAssetMenu(fileName = "New wave Setting",menuName = "Wave")]
+public class WaveSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float TimeInSeconds;
+    public WaveComponent[] WaveComponents;
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Serializable]
+public class WaveComponent
+{
+    public EnemyDatas enemy;
+    public int number;
+
 }
